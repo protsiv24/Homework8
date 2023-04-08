def6c334e0e08a13060ccc9a71fc631bc9f9f273
protsiv24 <protsiv12@gmail.com>
2023-04-08 16:20:31 +0300

    add third part of code

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ def mane_function(s):
     i = len(s) - 1
     while i >= 0:
         s2 = s2 + s[i]
+        i = i - 1
+        return s2
+    s1 = "qwerty"
+    s2 = mane_function(s1)  # s2 = 'ytrewq'
