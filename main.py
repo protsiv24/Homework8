53b2786b9725b11e8d0b02e9f2ead29062f7d9e6
protsiv24 <protsiv12@gmail.com>
2023-04-08 16:31:29 +0300

    add second part of code

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def mane_function(s):
       even_numbers = []
       for num in range(1, 21):
         if num % 2 == 0:
+            even_numbers.append(num)
+        return even_numbers
+    result = test_function()
+    print(result)
 
 
-    
