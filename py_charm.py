ad12129305f9f13811b809fc5e8056a1d33ba595
protsiv24 <protsiv12@gmail.com>
2023-04-08 16:51:13 +0300

    add new code

--- py_charm.py
+++ py_charm.py
@@ -4,3 +4,12 @@ def count_same_digits(number):
         digit = number % 10
         digit_counts[digit] += 1
         number //= 10
+
+def test_function():
+    even_numbers = []
+    for num in range(1, 21):
+        if num % 2 == 0:
+            even_numbers.append(num)
+    return even_numbers
+result = test_function()
+print(result)
